406a245e1d70fadaeaafc94313c7f608988355cd
Mikaelwichita <migu.leyton@duocuc.cl>
2024-10-08 19:49:36 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor() {}
 
+  ngOnInit() {
+    // Esperar 2 segundos antes de mostrar el logo
+    setTimeout(() => {
+      const logo = document.querySelector('.logo') as HTMLElement;
+      if (logo) {
+        logo.style.opacity = '1'; // Cambia la opacidad del logo a 1 para hacerlo visible
+      }
+
+      // Esperar 1 segundo más y luego mostrar la sección de login
+      setTimeout(() => {
+        const loginSection = document.querySelector('.login-section');
+        if (loginSection) {
+          loginSection.classList.add('show'); // Agregar clase para mostrar el formulario
+        }
+      }, 1000); // 1000 ms = 1 segundo
+
+    }, 2000); // 2000 ms = 2 segundos
+  }
 }
