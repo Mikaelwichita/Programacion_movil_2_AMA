a3deb73d3eb8325ff0b043d9be8b214b39daa990
Mikaelwichita <migu.leyton@duocuc.cl>
2024-10-29 07:32:04 -0300

    Actualizacion 4
    Se actualizaron cositas en el home jeje

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { WeatherService } from '../services/weather.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,9 +10,19 @@ import { WeatherService } from '../services/weather.service';
 })
 export class HomePage {
   weatherData: any;
-  city: string = 'Santiago';
+  city: string = 'San Joaquin';
+  role: string = '';
+  passengerCapacity: number = 1;
+  startPoint: string = '';
+  endPoint: string = '';
+  tripCost: number | null = null;
+  readonly conversionRate = 900; 
 
-  constructor(private weatherService: WeatherService, private router: Router) {}
+  constructor(
+    private weatherService: WeatherService,
+    private router: Router,
+    private alertController: AlertController
+  ) {}
 
   async getWeather() {
     try {
@@ -23,5 +34,42 @@ export class HomePage {
 
   navigateTo(page: string) {
     this.router.navigate([page]);
+  }
+
+  onRoleChange() {
+    if (this.role === 'conductor') {
+      this.passengerCapacity = 1;
+    }
+    this.updateTripCost();
+  }
+
+  updateTripCost() {
+    if (this.role && this.startPoint && this.endPoint && (this.role === 'pasajero' || this.passengerCapacity > 0)) {
+      const randomCLP = Math.floor(Math.random() * (45000 - 9000 + 1)) + 9000; 
+      this.tripCost = randomCLP; 
+    } else {
+      this.tripCost = null;
+    }
+  }
+  
+  
+
+  async startTrip() {
+    if (!this.startPoint || !this.endPoint || !this.role || (this.role === 'conductor' && this.passengerCapacity <= 0)) {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Por favor, completa todos los campos requeridos antes de iniciar el viaje.',
+        buttons: ['OK'],
+      });
+      await alert.present();
+      return;
+    }
+
+    const alert = await this.alertController.create({
+      header: 'Viaje Iniciado',
+      message: `Tu viaje de ${this.startPoint} a ${this.endPoint} ha comenzado. Precio estimado: $${this.tripCost} CLP.`,
+      buttons: ['OK'],
+    });
+    await alert.present();
   }
 }
