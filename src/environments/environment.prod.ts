4f1dfe470f5288b97fc0b332aeb5cd0878fc98ec
Mikaelwichita <migu.leyton@duocuc.cl>
2024-12-01 11:28:40 -0300

    PAL IONIC STUDIO
    Se agregaron estos codes (apk) para que corra en android studio y aplicaciones u.u

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true ,
+  mapboxApiKey: 'pk.eyJ1IjoibWlndWRlc3Ryb3llcjEyIiwiYSI6ImNtM3hybG9zNjBzZ3cydm14d3ppOHF6ejYifQ.GKz5RZkRybyG18kTAQDU6Q'
+
 };
